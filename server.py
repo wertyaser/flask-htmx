b6b3873a7d2ead78786fb1e5c5b23f80c2e030aa
leonielNacman <nacmanleonielgarin@gmail.com>
2024-04-04 22:14:46 +0800

    first commit

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+# flask --app server run --debug --port 3000
