55d14f5686faa618b22a2ea3598c5b6efa7d0916
leonielNacman <nacmanleonielgarin@gmail.com>
2024-04-04 23:35:34 +0800

    Python bytecode compiled files for server module have been updated: `server.cpython-31

--- server.py
+++ server.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('home/index.html')
 
+@app.route("/posts")
+def posts():
+    return render_template('posts/index.html')
 # flask --app server run --debug --port 5000
