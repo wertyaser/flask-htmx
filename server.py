29476c7683f0feb6bb955d1dddfda5bbeba65dff
wertyaser <nacmanleonielgarin@gmail.com>
2024-04-05 13:20:05 +0800

    Changes to be committed: 	modified:   __pycache__/server.cpython-312.pyc 	modified:   server.py 	modified:   templates/_macros.html 	new file:   templates/cashier/index.html 	modified:   templates/home/index.html 	renamed:    templates/posts/index.html -> templates/users/index.html

--- server.py
+++ server.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template('home/index.html')
 
-@app.route("/posts")
-def posts():
-    return render_template('posts/index.html')
+@app.route("/users")
+def users():
+    return render_template('users/index.html')
+
+@app.route("/cashier")
+def cashier():
+    return render_template('cashier/index.html')
+
+# @app.route("/logout")
+# def logout():
+#     return render_template('logout/index.html')
+
 # flask --app server run --debug --port 5000
